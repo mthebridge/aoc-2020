7eefce7b5845e1f06e1c2988a452599111dead58
Mark Thebridge <mark.thebridge@metaswitch.com>
2020-12-03 09:34:18 +0000

    Small tweak

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,5 +1,6 @@
 use std::collections::HashSet;
 
+
 #[derive(Debug, Clone)]
 struct Map {
     width: usize,
@@ -38,22 +39,31 @@ impl Map {
 
     fn count_trees_on_slope(&self, x_change: usize, y_change: usize) -> usize {
         (0..)
-            .step_by(x_change)
-            .zip((0..self.height).step_by(y_change))
-            .fold(0, |count, this| {
-                if self.trees.contains(&(this.0 % self.width, this.1)) {
-                    count + 1
-                } else {
-                    count
-                }
-            })
+        .step_by(x_change)
+        .zip((0..self.height).step_by(y_change))
+        .fold(0, |count, this| {
+            if self.trees.contains(&(this.0 % self.width, this.1)) {
+                count + 1
+            } else {
+                count
+            }
+        })
     }
 }
+
+const ADDITIONAL_SLOPES: [(usize, usize); 4] = [(1, 1), (5, 1), (7, 1), (1, 2)];
 
 fn main() {
     let input = include_str!("./input.txt");
     let map = Map::from_str(input).expect("Failed to parse input!");
-    println!("Part 1: {} trees on path", map.count_trees_on_slope(3, 1));
+    let part1_treecount = map.count_trees_on_slope(3, 1);
+    println!("Part 1: {} trees on path", part1_treecount);
+    let part2_product = ADDITIONAL_SLOPES
+        .iter()
+        .fold(part1_treecount, |product, (x, y)| {
+            product * map.count_trees_on_slope(*x, *y)
+        });
+    println!("Part 2: Product of all five answers: {}", part2_product);
 }
 
 #[cfg(test)]
