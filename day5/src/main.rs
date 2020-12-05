2110a88e6e843a3a282f9b232a78c4fe233247a3
Mark Thebridge <mark.thebridge@gmail.com>
2020-12-05 11:50:25 +0000

    Day 5

---
+++ day5/src/main.rs
@@ -0,0 +1,57 @@
+use std::convert::TryFrom;
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+struct BoardingPass {
+    row: u8,
+    seat: u8,
+}
+
+impl BoardingPass {
+    fn from_str(input: &str) -> Self {
+        if input.len() != 10 {
+            panic!("Invalid input line - too long");
+        }
+        let (rows, seat) = input.split_at(7);
+
+        let bin_parser = |s: &str, lower: char, upper: char| -> u8 {
+            s.chars()
+                .rev()
+                .map(|c| match c {
+                    x if x == lower => 0,
+                    x if x == upper => 1,
+                    _ => panic!("Invalid input char"),
+                })
+                .enumerate()
+                .fold(0u8, |acc, (idx, next)| {
+                    acc + (next * 2u8.pow(u32::try_from(idx).unwrap()))
+                })
+        };
+
+        BoardingPass {
+            row: bin_parser(rows, 'F', 'B'),
+            seat: bin_parser(seat, 'L', 'R'),
+        }
+    }
+
+    fn seat_id(&self) -> u16 {
+        u16::from(self.row) * 8 + u16::from(self.seat)
+    }
+}
+
+fn main() {
+    let input = include_str!("./input.txt");
+    let get_seat_ids = || input.lines().map(|l| BoardingPass::from_str(l).seat_id());
+    let max_id = get_seat_ids().max().expect("Empty list");
+    println!("Part 1: Max Seat ID {}", max_id);
+    let mut seat_ids = get_seat_ids().collect::<Vec<_>>();
+    // Find the missing ID.
+    // Easiest way to do this is sort the list of IDs, then walk them looking for the first
+    // where the previous entry is not one lower.
+    // That entry in the seat_ids vector is one higher than the missing iD.
+    seat_ids.sort_unstable();
+    let mut candidates = (1..seat_ids.len())
+        .filter(|&idx| seat_ids[idx] >= 8 && seat_ids[idx - 1] != seat_ids[idx] - 1);
+    let my_seat = seat_ids[candidates.next().expect("No missing seat")] - 1;
+    assert!(candidates.next().is_none());
+    println!("Part 2: Missing Seat ID: {}", my_seat);
+}
