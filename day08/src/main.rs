99efb039bc2b22ff03c527c43d90cfed82ecd8e0
Mark Thebridge <mark.thebridge@metaswitch.com>
2020-12-08 16:06:14 +0000

    Day8 part1

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -1,3 +1,60 @@
+use std::collections::HashSet;
+
+enum Instruction {
+    Nop(i32),
+    Acc(i32),
+    Jump(i32),
+}
+
+fn parse_line(line: &str) -> Instruction {
+    let mut words = line.split_whitespace();
+    let itype = words.next().expect("Empty line");
+    let arg = words
+        .next()
+        .expect("No argument")
+        .parse()
+        .expect("Invalid arg");
+    assert!(words.next().is_none());
+    match itype {
+        "acc" => Instruction::Acc(arg),
+        "jmp" => Instruction::Jump(arg),
+        "nop" => Instruction::Nop(arg),
+        _ => panic!("invalid line"),
+    }
+}
+
+fn run_until_loop(instructions: &[Instruction]) -> i32 {
+    let mut acc = 0;
+    let mut idx = 0;
+    let mut seen = HashSet::new();
+    loop {
+        if !seen.insert(idx) {
+            return acc;
+        }
+        match instructions[idx] {
+            Instruction::Nop(_) => idx += 1,
+            Instruction::Jump(arg) => {
+                if arg > 0 {
+                    idx += arg as usize
+                } else {
+                    idx -= (-arg) as usize
+                }
+            }
+            Instruction::Acc(arg) => {
+                acc += arg;
+                idx += 1
+            }
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("./input.txt");
+
+    let instructions = input.lines().map(parse_line).collect::<Vec<_>>();
+    println!(
+        "Part 1: Acc is {} after repeat",
+        run_until_loop(&instructions)
+    );
+    println!("Part 2");
 }
