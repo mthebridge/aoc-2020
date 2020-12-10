75272724d02ca49dd640602c053a59c3d9060b09
Mark Thebridge <mark.thebridge@gmail.com>
2020-12-10 19:49:52 +0000

    Refactor

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,3 +1,13 @@
+fn get_differences(voltages: &[u32]) -> impl Iterator<Item = u32> + Clone + '_ {
+    (1..voltages.len()).map(move |idx| {
+        let diff = voltages[idx] - voltages[idx - 1];
+        if diff < 1 || diff > 3 {
+            panic!("Invalid difference")
+        };
+        diff
+    })
+}
+
 fn main() {
     let input = include_str!("./input.txt");
 
@@ -10,13 +20,7 @@ fn main() {
     // Add the zero-index.
     voltages.insert(0, 0);
 
-    let diffs = (1..voltages.len()).map(|idx| {
-        let diff = voltages[idx] - voltages[idx - 1];
-        if diff < 1 || diff > 3 {
-            panic!("Invalid difference")
-        };
-        diff
-    });
+    let diffs = get_differences(&voltages);
 
     let ones = diffs.clone().filter(|&d| d == 1).count();
     let threes = diffs.filter(|&d| d == 3).count() + 1;
@@ -27,5 +31,10 @@ fn main() {
         threes,
         ones * threes
     );
+
+    // Ugh.
+
+    let min_length = voltages.len() / 3;
+
     println!("Part 2");
 }
