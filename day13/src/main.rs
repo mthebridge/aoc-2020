eca3a41b77d7ddccb1c1e41272708a05a6c48c18
Mark Thebridge <mark.thebridge@metaswitch.com>
2020-12-16 21:24:10 +0000

    Day14 done, continue day13

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -10,11 +10,9 @@ fn main() {
         .next()
         .unwrap()
         .split(',')
-        .filter(|&s| s != "x")
-        .map(|s| s.parse::<u32>().unwrap())
-        .collect::<Vec<_>>();
+        .map(|s| s.parse::<u32>().ok());
     // Want the bus whose multiple is closest to (on ar after) earliest.
-    let mut part1_buses = buses.clone();
+    let mut part1_buses = buses.clone().filter_map(|b| b).collect::<Vec<_>>();
     part1_buses.sort_unstable_by_key(|&k| wait_time(k, earliest));
     let my_bus = part1_buses[0];
     let my_wait_time = wait_time(my_bus, earliest);
@@ -25,4 +23,31 @@ fn main() {
         my_wait_time,
         my_bus * my_wait_time
     );
+
+    // For [aprt2: find the first time]
+    let mut part2_buses = buses.clone().
+    enumerate().filter_map(|(idx, bus)| {
+        if let Some(b) = bus {
+            println!("Must be {} modulo {}", idx, b);
+            Some((idx, b))
+        } else {
+            None
+        }
+    }).collect::<Vec<_>>();
+    part2_buses.sort_unstable_by_key(|&k| k.1);
+
+    type Candidate = Box<dyn Iterator<Item=usize>>;
+    let mut candidates: Option<Candidate> = None;
+    for r in part2_buses.rev() {
+        if let Some(existing) = candidates {
+            Some(Box::new(existing.filter(|c| r.(c).is_some()))as Candidate)
+        } else {
+            Some(Box::new(r.map(|(_, v)|)) as Candidate)
+        }
+    }
+
+    // Need to find the first value that appears in each
+
+
+    println!("Part 2; {}", candidates.unwrap().next().unwrap());
 }
--- day14/src/main.rs
+++ day14/src/main.rs
@@ -1,8 +1,28 @@
 use std::collections::HashMap;
 
-fn run_initialization(input: &str) -> HashMap<u64, u64> {
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Ver {
+    V1,
+    V2,
+}
+
+fn apply_mask(mask: &[Option<bool>], base: u64) -> u64 {
+    mask.iter().enumerate().fold(base, |acc, (idx, &v)| {
+        match v {
+            // Set this bit to 1
+            Some(true) => acc | (1 << idx),
+            // Clear this bit
+            Some(false) => acc & !(1 << idx),
+            // Ignore anything else
+            None => acc,
+        }
+    })
+}
+
+fn run_initialization(input: &str, version: Ver) -> HashMap<u64, u64> {
     let mut memory = HashMap::new();
-    let mut cur_mask = HashMap::new();
+    let mut cur_mask = Vec::with_capacity(36);
+    let mut masks = Vec::new();
     for line in input.lines() {
         match line.split_whitespace().next() {
             Some("mask") => {
@@ -12,14 +32,38 @@ fn run_initialization(input: &str) -> HashMap<u64, u64> {
                     .unwrap()
                     .chars()
                     .rev()
-                    .enumerate()
-                    .filter_map(|(idx, c)| match c {
+                    .map(|c| match c {
                         'X' => None,
-                        '1' => Some((idx, true)),
-                        '0' => Some((idx, false)),
+                        '1' => Some(true),
+                        '0' => Some(false),
                         _ => panic!("Bad mask"),
                     })
                     .collect();
+
+                if version == Ver::V2 {
+                    // Generate all possible masks.
+                    masks = vec![];
+                    masks.push(cur_mask.clone());
+                    for (idx, val) in cur_mask.iter().enumerate() {
+                        for mask in &mut masks {
+                            mask[idx] = match val {
+                                Some(true) => Some(true),
+                                Some(false) => None,
+                                None => Some(false),
+                            }
+                        }
+                        if val.is_none() {
+                            let mut new_masks = masks.clone();
+                            for mask in &mut masks {
+                                mask[idx] = Some(true);
+                            }
+                            for mask in &mut new_masks {
+                                mask[idx] = Some(false);
+                            }
+                            masks.extend(new_masks.into_iter());
+                        }
+                    }
+                }
             }
             Some(x) if x.starts_with("mem") => {
                 // Format is mem[addr] = val
@@ -35,16 +79,18 @@ fn run_initialization(input: &str) -> HashMap<u64, u64> {
                     .parse::<u64>()
                     .unwrap();
                 // Mask the val
-                let masked = cur_mask.iter().fold(val, |acc, (&idx, &v)| {
-                    if v {
-                        // Set this bit to 1
-                        acc | (1 << idx)
-                    } else {
-                        // Clear this bit
-                        acc & !(1 << idx)
+                match version {
+                    Ver::V1 => {
+                        let masked = apply_mask(&cur_mask, val);
+                        memory.insert(addr, masked);
                     }
-                });
-                memory.insert(addr, masked);
+                    Ver::V2 => {
+                        for mask in &masks {
+                            let masked = apply_mask(&mask, addr);
+                            memory.insert(masked, val);
+                        }
+                    }
+                }
             }
             _ => panic!("Invalid line"),
         }
@@ -54,8 +100,22 @@ fn run_initialization(input: &str) -> HashMap<u64, u64> {
 
 fn main() {
     let input = include_str!("./input.txt");
-    let mem = run_initialization(input);
-
+    let mem = run_initialization(input, Ver::V1);
     let sum: u64 = mem.values().sum();
     println!("Part 1: {}", sum);
+
+    let mem = run_initialization(input, Ver::V2);
+    let sum: u64 = mem.values().sum();
+    println!("Part 2: {}", sum);
+}
+
+#[test]
+fn test_part2() {
+    let input = r#"mask = 000000000000000000000000000000X1001X
+mem[42] = 100
+mask = 00000000000000000000000000000000X0XX
+mem[26] = 1"#;
+    let mem = run_initialization(input, Ver::V2);
+    let sum: u64 = mem.values().sum();
+    assert_eq!(sum, 208);
 }
