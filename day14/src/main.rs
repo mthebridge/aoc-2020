9c92776e6e03133c500e5758308635bfa094d338
Mark Thebridge <mark.thebridge@metaswitch.com>
2020-12-16 09:25:54 +0000

    day14pt1 and day15

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -1,3 +1,61 @@
+use std::collections::HashMap;
+
+fn run_initialization(input: &str) -> HashMap<u64, u64> {
+    let mut memory = HashMap::new();
+    let mut cur_mask = HashMap::new();
+    for line in input.lines() {
+        match line.split_whitespace().next() {
+            Some("mask") => {
+                cur_mask = line
+                    .split_whitespace()
+                    .nth(2)
+                    .unwrap()
+                    .chars()
+                    .rev()
+                    .enumerate()
+                    .filter_map(|(idx, c)| match c {
+                        'X' => None,
+                        '1' => Some((idx, true)),
+                        '0' => Some((idx, false)),
+                        _ => panic!("Bad mask"),
+                    })
+                    .collect();
+            }
+            Some(x) if x.starts_with("mem") => {
+                // Format is mem[addr] = val
+                let addr = x
+                    .trim_start_matches("mem[")
+                    .trim_end_matches("]")
+                    .parse::<u64>()
+                    .unwrap();
+                let val = line
+                    .split_whitespace()
+                    .nth(2)
+                    .unwrap()
+                    .parse::<u64>()
+                    .unwrap();
+                // Mask the val
+                let masked = cur_mask.iter().fold(val, |acc, (&idx, &v)| {
+                    if v {
+                        // Set this bit to 1
+                        acc | (1 << idx)
+                    } else {
+                        // Clear this bit
+                        acc & !(1 << idx)
+                    }
+                });
+                memory.insert(addr, masked);
+            }
+            _ => panic!("Invalid line"),
+        }
+    }
+    memory
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("./input.txt");
+    let mem = run_initialization(input);
+
+    let sum: u64 = mem.values().sum();
+    println!("Part 1: {}", sum);
 }
--- day15/src/main.rs
+++ day15/src/main.rs
@@ -1,3 +1,27 @@
+use std::collections::HashMap;
+
+fn run_game_until_step(starting: &[usize], turns: usize) -> usize {
+    let start_size = starting.len();
+    let mut last_val = starting[start_size - 1];
+    let mut data = starting[..start_size - 1].iter().enumerate().map(|(i, &v)| (v,i)).collect::<HashMap<_, _>>();
+
+    for idx in starting.len()..turns {
+        // Find the last time the previous number was spoken
+        let prev = data.entry(last_val).or_insert(idx - 1);
+        last_val = idx - 1 - *prev;
+        *prev = idx - 1;
+    }
+    last_val
+}
+
 fn main() {
-    println!("Hello, world!");
+    let starting_numbers: [usize; 7] = [11,18,0,20,1,7,16];
+    println!("Part1: {}", run_game_until_step(&starting_numbers, 2020));
+    println!("Part2: {}", run_game_until_step(&starting_numbers, 30_000_000));
+}
+
+#[test]
+fn test_sample() {
+    assert_eq!(run_game_until_step(&[0, 3, 6], 9), 4);
+    assert_eq!(run_game_until_step(&[0, 3, 6], 2020), 436);
 }
